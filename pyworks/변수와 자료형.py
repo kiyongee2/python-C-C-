08f1028d5a73bdd6dcae257bedf0d92a9ac6fbf0
kiyongee2 <kiyongee2@gmail.com>
2024-12-22 19:59:48 +0900

    변수와 자료형

---
+++ pyworks/변수와 자료형.py
@@ -0,0 +1,65 @@
+"""
+print("서울특별시")
+print(10)
+"""
+
+# 산술 연산
+"""
+print(10 + 20)
+print(10 - 20)
+print(10 * 20)
+print(10 / 20)
+"""
+
+# a라는 변수에 10을 저장함(대입 연산자-'=')
+a = 10
+print("a =", a)
+
+a = 20
+print("a =", a)
+
+b = 30
+
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+
+# 자료형
+print(type(a))
+print(type(b))
+
+season = "겨울"
+print(season)
+print(type(season))
+
+"""
+# 변수 이름 작성시 오류
+  1. 숫자로 시작하면 오류,
+  2. 이름에 공백이 있으면 오류,
+  3. 예약어 - if, class, def
+"""
+# 2season = "봄"
+# print(2season)
+
+# season 3 = "가을"
+# print(season 3)
+
+# if = 100
+print(if)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
