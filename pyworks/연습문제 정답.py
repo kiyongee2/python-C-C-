a9d9c9b5d1af6aa5cb62b6a3342f76d8d4545844
kiyongee2 <kiyongee2@gmail.com>
2024-12-27 12:22:43 +0900

    연습문제

---
+++ pyworks/연습문제 정답.py
@@ -0,0 +1,16 @@
+# 연습 1
+user = "jerry"
+age = 17
+print("user =", user);
+print("age =", age)
+
+# 연습 2
+bread = 30
+person = 4
+
+몫 = bread // person
+나머지 = bread % person
+
+print("각자 가져간 빵의 개수 =", 몫);
+print("남은 빵의 개수 =", 나머지)
+
