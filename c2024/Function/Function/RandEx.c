393e0b69c4882753d7abfc151e968ebf50b4e7e8
kiyongee2 <kiyongee2@gmail.com>
2025-02-22 07:18:37 +0900

    문자열 함수

--- c2024/Function/Function/RandEx.c
+++ c2024/Function/Function/RandEx.c
@@ -41,7 +41,7 @@
 //
 //	switch (n)
 //	{
-//	case 0: printf("가위\n"); break;
+//    case 0: printf("가위\n"); break;
 //	case 1: printf("바위\n"); break;
 //	case 2: printf("보\n"); break;
 //	default: printf("없음\n"); break;
--- c2024/Function/Function/Strcmp.c
+++ c2024/Function/Function/Strcmp.c
@@ -1,32 +1,32 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <string.h>  // strcpy(), strlen(), strcmp() 사용을 위해 필요
-
-int main() {
-
-    //문자열 복사 - strcpy(배열, 문자열)
-    //char str[] = "Good Luck!!";
-    char msg[20];
-
-    //printf("%s\n", strcpy(msg, str));
-    printf("%s\n", strcpy(msg, "Good Luck!!"));
-
-    char greet1[20] = "hello";
-    char greet2[20];
-
-    //문자열의 길이
-    printf("%d\n", strlen(greet1));
-
-    printf("문자열을 입력하세요: ");
-    scanf("%s", greet2);
-
-    // 문자열 비교
-    if (strcmp(greet1, greet2) == 0) {
-        printf("문자열이 일치합니다!\n");
-    }
-    else {
-        printf("문자열이 일치하지 않습니다.\n");
-    }
-
-    return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <string.h>  // strcpy(), strlen(), strcmp() 사용을 위해 필요
+//
+//int main() {
+//
+//    //문자열 복사 - strcpy(배열, 문자열)
+//    //char str[] = "Good Luck!!";
+//    char msg[20];
+//
+//    //printf("%s\n", strcpy(msg, str));
+//    printf("%s\n", strcpy(msg, "Good Luck!!"));
+//
+//    char greet1[20] = "hello";
+//    char greet2[20];
+//
+//    //문자열의 길이
+//    printf("%d\n", strlen(greet1));
+//
+//    printf("문자열을 입력하세요: ");
+//    scanf("%s", greet2);
+//
+//    // 문자열 비교
+//    if (strcmp(greet1, greet2) == 0) {
+//        printf("문자열이 일치합니다!\n");
+//    }
+//    else {
+//        printf("문자열이 일치하지 않습니다.\n");
+//    }
+//
+//    return 0;
+//}
